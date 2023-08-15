b49df77b277dcda10889e677f63572af85043b69
Patricia Soldo <140009701+PatiSoldo@users.noreply.github.com>
2023-08-15 20:40:07 -0300

    Update Menu.java

--- PROJETO GUIA/Menu.java
+++ PROJETO GUIA/Menu.java
@@ -1,6 +1,10 @@
 package conta;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import conta.controller.ContaController;
 import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
@@ -9,37 +13,22 @@ import conta.util.Cores;
 
 
 public class Menu {
+	
+	public static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-	//Teste da Classe Conta
-	Conta c1 = new Conta(1, 123,1,"Adriana", 10000.0f);
-	c1.visualizar();
-	c1.sacar(12000.0f);
-	c1.visualizar();
-	c1.depositar(5000.0f);
-	c1.visualizar();
+		ContaController contas = new ContaController();
+		
 	
-	//Teste da Classe Conta Corrente
-	ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
-	cc1.visualizar();
-	cc1.sacar(12000.0f);
-	cc1.visualizar();
-	cc1.depositar(5000.0f);
-	cc1.visualizar();
-	
-	//Teste da Classe Conta Poupança
-	ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
-	cp1.visualizar();
-    cp1.sacar(1000.0f);
-	cp1.visualizar();
-	cp1.depositar(5000.0f);
-	cp1.visualizar();
+	int opcao, numero, agencia, tipo, aniversario;
+	String titular;
+	float saldo, limite;
 	
 		
 		
      Scanner leia= new Scanner(System.in);
-     int opcao;
+     int opcao1;
      
      while(true) {
     	 
@@ -64,20 +53,60 @@ public class Menu {
     	 System.out.println("Entre com a opção desejada:                            ");
     	 System.out.println("                                                       "+Cores.TEXT_RESET);
     	 
-    	 opcao = leia.nextInt();
+
+    	 try {
+				opcao1 = leia.nextInt();
+			}catch(InputMismatchException e){
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao1=0;
+			}
+
     	 
-    	 if(opcao==9) {
+    	 if(opcao1==9) {
     		 System.out.println(Cores.TEXT_WHITE_BOLD +"\nBanco do Brazil com Z - O seu Futuro começa aqui!");
     		 leia.close();
     		 System.exit(0);
     	 }
-        switch(opcao) {
+        switch(opcao1) {
         case 1 : 
         	System.out.println(Cores.TEXT_WHITE_BOLD +"Criar Conta \n\n");
+        	
+        	System.out.println("Digite o Número da Agência: ");
+        	agencia = leia.nextInt();
+        	System.out.println("Digite o Nome do Titular: ");
+        	leia.skip("\\R");
+        	titular = leia.nextLine();
+        	
+        	do {
+        		System.out.println("Digite o Tipo da Conta  (1-CC ou 2-CP): ");
+        		tipo = leia.nextInt();
+        	}while(tipo < 1 && tipo >2);
+        	
+        	System.out.println("Digite o Saldo da Conta (R$): ");
+        	saldo = leia.nextFloat();
+        	
+        	switch(tipo) {
+        	case 1 ->{
+        		System.out.println("Digite o Limite de crédito (R$): ");
+        		limite = leia.nextFloat();
+        		contas.cadastrar(new ContaCorrente(contas.gerarNumero(),agencia, tipo, titular,saldo, limite));
+        	}
+        	case 2 -> {
+        		System.out.println("Digite o dia do Aniversário da Conta: ");
+        		aniversario = leia.nextInt();
+        		contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+        		
+               }
+        	}
+        	keyPress();
         	break;
+        			
         	
         case 2 :
         	System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as Contas \n\n");
+        	contas.listarTodas();
+        	keyPress();
         	break;
         
         case 3 :
@@ -110,8 +139,19 @@ public class Menu {
         }
      }
      
-	}
-		
 }
+		
+	public static void keyPress() {
 
+		try {
 
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
+}
